e337b8f8e92604fbf497f392346253eccef52b9f
visd <vishaldaga10@gmail.com>
2024-03-31 20:11:40 +0530

    Init client project

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import StudentForm from "./pages/StudentForm/StudentForm.jsx";
+import './App.css'
+
+function App() {
+  return (
+    <>
+        <StudentForm />
+    </>
+  )
+}
+
+export default App
---
+++ client/src/pages/StudentForm/StudentForm.jsx
@@ -0,0 +1,9 @@
+import React, {useEffect, useState} from "react";
+import "./styles.css";
+
+
+function StudentForm() {
+    return <>1</>
+}
+
+export default StudentForm;
